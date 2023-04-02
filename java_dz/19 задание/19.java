1b69e842c7f86f64b96a92fcdbc240819f334835
Vanshev Mihail <112972907+Vanshve-Mihail@users.noreply.github.com>
2023-04-02 20:34:24 +0300

    Create 19.java

---
+++ java_dz/19 задание/19.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class dz {
+    public static void Method() {
+        System.out.println(func(GetNumberTask19()));
+    }
+    public static int func(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else if (n % 2 == 0) { //n / 2 == (int) n / 2
+            return func(n / 2);
+        } else {
+            return func((n - 1)/ 2) + func(((n - 1) / 2) + 1 );
+        }
+    }
+    public static int GetNumberTask19() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите число: ");
+        return in.nextInt();
+    }
+}
