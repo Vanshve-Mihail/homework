8ac123843691393c4ad919587cf314eba9d10327
Vanshev Mihail <112972907+Vanshve-Mihail@users.noreply.github.com>
2023-04-10 17:47:33 +0300

    Create Main.java

---
+++ java_dz/22 задание/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle();
+        System.out.println("Площадь равна: " + triangle.calculateTriangleArea(4, 6, 8));
+    }
+}
