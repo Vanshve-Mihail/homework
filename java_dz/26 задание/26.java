3c37eefc5da2b27bd7784400ac090c11b13738cb
Vanshev Mihail <112972907+Vanshve-Mihail@users.noreply.github.com>
2023-05-14 19:17:04 +0300

    26.java

---
+++ java_dz/26 задание/26.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите строку: ");
+        String str = scanner.nextLine();
+        String longestPalindrome = findLongestPalindrome(str);
+        System.out.println(longestPalindrome);
+    }
+    public static String findLongestPalindrome(String str) {
+        if (str == null || str.length() == 0) {
+            return "";
+        }
+        int len = str.length();
+        boolean[][] dp = new boolean[len][len];
+        int maxLen = 0;
+        int startIndex = 0;
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
+            maxLen = 1;
+        }
+        for (int i = 0; i < len - 1; i++) {
+            if (str.charAt(i) == str.charAt(i+1)) {
+                dp[i][i+1] = true;
+                maxLen = 2;
+                startIndex = i;
+            }
+        }
+        for (int k = 3; k <= len; k++) {
+            for (int i = 0; i < len - k + 1; i++) {
+                int j = i + k - 1;
+                if (str.charAt(i) == str.charAt(j) && dp[i+1][j-1]) {
+                    dp[i][j] = true;
+                    if (k > maxLen) {
+                        maxLen = k;
+                        startIndex = i;
+                    }
+                }
+            }
+        }
+        return str.substring(startIndex, startIndex + maxLen) + " (" + maxLen + ")";
+    }
+}
