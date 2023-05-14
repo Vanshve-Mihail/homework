3ea201ebd0131a12031a278696c382dad899b3e0
Vanshev Mihail <112972907+Vanshve-Mihail@users.noreply.github.com>
2023-05-14 19:10:46 +0300

    Create 25.java

---
+++ java_dz/25 задание/25.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите M: ");
+        int M = scanner.nextInt();
+
+        System.out.print("Введите N: ");
+        int N = scanner.nextInt();
+
+        System.out.print("Кол-во делителей: ");
+        int numDivisors = scanner.nextInt();
+
+        System.out.print("Делители: ");
+        int[] divisors = new int[numDivisors];
+        for (int i = 0; i < numDivisors; i++) {
+            divisors[i] = scanner.nextInt();
+        }
+        int sum1 = 0;
+        for (int divisor : divisors) {
+            int sum = 0;
+            for (int num = M; num <= N; num++) {
+                if (num % divisor == 0) {
+                    sum += num;
+                }
+            }
+            sum1 += sum;
+        }
+        System.out.println("Сумма делителей: " + sum1);
+    }
+}
