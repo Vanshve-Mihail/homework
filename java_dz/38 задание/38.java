7b857b8cd1c7ab3d4407371d4e45fc24775b2638
Vanshev Mihail <112972907+Vanshve-Mihail@users.noreply.github.com>
2023-05-15 18:51:53 +0300

    38.java

---
+++ java_dz/38 задание/38.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main1 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите текст: ");
+        String input = scanner.nextLine();
+
+        input = input.replaceAll("[^\\p{L}\\s]+", "");
+
+        String[] words = input.split("\\s+");
+        StringBuilder sb = new StringBuilder();
+        for (String word : words) {
+            sb.append(word.substring(1)).append(word.charAt(0)).append("Ауч ");
+        }
+
+        System.out.println(sb.toString().trim());
+    }
+}
