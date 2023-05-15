72258c28614d7a7859e909d7b25c55a3fb31b755
Vanshev Mihail <112972907+Vanshve-Mihail@users.noreply.github.com>
2023-05-15 19:59:37 +0300

    33.java

---
+++ java_dz/33 задание/33.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Кол-во палочек: ");
+        long n = scanner.nextLong();
+
+        long sashaSticks = n - sticksLeft(n, "Таня");
+        long tanyaSticks = sticksLeft(n, "Таня");
+
+        System.out.println("Кол-во палочек Саши " + sashaSticks);
+        System.out.println("Кол-во палочек Тани " + tanyaSticks);
+    }
+
+    public static long sticksLeft(long n, String player) {
+        if (n == 1) {
+            if (player.equals("Tanya")) {
+                return 1;
+            } else {
+                return 0;
+            }
+        } else if (n % 2 == 0) {
+            if (player.equals("Таня")) {
+                long half = n / 2;
+                if (half % 2 == 0) {
+                    return sticksLeft(half, "Саша") + half;
+                } else {
+                    return sticksLeft(half + 1, "Саша") + half;
+                }
+            } else {
+                return sticksLeft(n - 1, "Таня");
+            }
+        } else {
+            return sticksLeft(n - 1, "Таня");
+        }
+    }
+}
